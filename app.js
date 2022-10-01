958d04569347055d33a00655c28b60567e19839c
InsÆne <70924817+RafaCEE@users.noreply.github.com>
2022-10-01 14:35:56 -0700

    primer chat

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-var express =  require('express');
-    app = express();
-    server = require('http').createServer(app);
-    io = require('socket.io').listen(server);
+const express = require('express')
+const app = express()
 
-server.listen(3000);
+const http = require('http')
+const { send } = require('process')
+const server = http.createServer(app)
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+	//console.log('a user connected')
+	// socket.on('disconnect', () => {
+	// 	console.log('user disconnected')
+	// })
+
+	// socket.on('chat message', (msg) => {
+	// 	io.emit('chat message', msg)
+	// 	console.log('message: ' + msg)
+	// })
+
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg)
+		console.log('message: ' + msg)
+	})	
+})
+
+app.get('/', (req, res) => {
+	//res.send('<h1>Aplication chat</h1>')
+	res.sendFile(__dirname + '/index.html')
+})
+
+server.listen(3000, () => {
+	console.log('Server is running on port http://localhost:3000')
+})
